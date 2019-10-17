7c2923d17ccdad3b693f81fcb9339c0e9d8c349d
fabio <fabgmad@gmail.com>
2019-10-17 20:33:57 -0300

    routes

--- app.js
+++ app.js
@@ -1,23 +1,40 @@
 require('dotenv').config();
 
-const bodyParser   = require('body-parser');
-const cookieParser = require('cookie-parser');
-const express      = require('express');
-const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
-const mongoose     = require('mongoose');
-const logger       = require('morgan');
-const path         = require('path');
+// Dependencies
 
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const express = require('express');
+const favicon = require('serve-favicon');
+const hbs = require('hbs');
+const mongoose = require('mongoose');
+const logger = require('morgan');
+const path = require('path');
+const session = require("express-session");
+const bcrypt = require("bcrypt");
+const bcryptSalt = 10;
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const flash = require("connect-flash");
+const MongoStore = require("connect-mongo")(session);
+
+// Require models
+
+
+// Mongoose
 
 mongoose
-  .connect('mongodb://localhost/hackathon-app', {useNewUrlParser: true})
+  .connect('mongodb://localhost/hackathon-app', {
+    useNewUrlParser: true
+  })
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
   .catch(err => {
-    console.error('Error connecting to mongo', err)
+    console.error('Error connecting to mongo', err);
   });
+
+// Debug
 
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
@@ -25,19 +42,23 @@ const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.
 const app = express();
 
 // Middleware Setup
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 
-// Express View engine setup
+// Express View Engine Setup
 
 app.use(require('node-sass-middleware')({
-  src:  path.join(__dirname, 'public'),
+  src: path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
   sourceMap: true
 }));
-      
+
+// Config Views
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -45,11 +66,67 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 
-
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
 
+// Session & Passport
 
+app.use(session({
+  secret: "food-products-project",
+  // cookie: { maxAge: 6000000 },
+  resave: true,
+  saveUninitialized: true,
+}));
+
+// Serialize & Deserialize
+
+passport.serializeUser((user, cb) => {
+  cb(null, user._id);
+});
+
+passport.deserializeUser((id, cb) => {
+  User.findById(id, (err, user) => {
+    if (err) {
+      return cb(err);
+    }
+    cb(null, user);
+  });
+});
+
+passport.use(new LocalStrategy({
+  passReqToCallback: true
+}, (req, username, password, next) => {
+  User.findOne({
+    username
+  }, (err, user) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return next(null, false, {
+        message: "Usuário incorreto"
+      });
+    }
+    if (!bcrypt.compareSync(password, user.password)) {
+      return next(null, false, {
+        message: "Senha incorreta"
+      });
+    }
+
+    return next(null, user);
+  });
+}));
+
+// Session
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Flash
+
+app.use(flash());
+
+// Routes
 
 const index = require('./routes/index');
 app.use('/', index);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,77 @@
-const express = require('express');
-const router  = express.Router();
+/* eslint-disable no-else-return */
+require('dotenv').config()
 
-/* GET home page */
+// ########################### Dependencies #######################
+
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+const bcrypt = require('bcrypt');
+const bcryptSalt = 10;
+const ensureLogin = require('connect-ensure-login');
+const mongoose = require('mongoose');
+// const multer = require("multer");
+const path = require('path');
+const axios = require('axios');
+
+// ########################### Require Models #######################
+
+
+
+// ########################### Main Page #######################
+
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+// ########################### Manage Roles #######################
+
+const checkAdmin  = checkRoles('admin');
+
+function checkRoles(role) {
+  return function(req, res, next) {
+    if (req.isAuthenticated() && req.user.role === role) {
+      return next();
+    } else {
+      res.redirect('/login');
+    }
+  };
+}
+
+// ########################### Function Ensure Authentication #######################
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  } else {
+    res.redirect('/login');
+  }
+}
+
+// ########################### Admin Login #######################
+
+router.get('/login', (req, res, next) => {
+  res.render('login');
+});
+
+router.post('/login', (req, res, next) => {
+  res.render('admin-page');
+});
+
+// ########################### Admin Logout #######################
+
+router.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/login');
+});
+
+// ########################### Admin Page #######################
+
+
+
+// ########################### Client - Sign Up #######################
+
+
+
 
 module.exports = router;
